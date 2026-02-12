4baf3a47c4a3e78239f45826032c37f0dd560d0d
yeldar.nuralin <nuralin.e@adamants.kz>
2026-02-12 23:15:19 +0500

    Task

--- users-write-service/src/main/java/com/epam/nuralin/yeldar/write_service/entity/UserEntity.java
+++ users-write-service/src/main/java/com/epam/nuralin/yeldar/write_service/entity/UserEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -21,8 +20,7 @@ import java.time.OffsetDateTime;
 public class UserEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
-    @SequenceGenerator(name = "users_seq", sequenceName = "users_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
